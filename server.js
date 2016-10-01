168a8d89ce9f3e99b348212b43dd8c4d2ad07234
HariharanG09 <valavanurguru@gmail.com>
2016-10-01 20:52:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,12 @@ counter=counter+1;
 res.send(counter.toString());
 });
 
-
+var names=[];
+app.get("/submit-name",function(req,res){
+var name=req.query.name;
+names.push(name);
+res.send(JSON.stringify(names));
+});
 
 
 
@@ -122,12 +127,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-var names=[];
-app.get("/submit-name",function(req,res){
-var name=req.query.name;
-names.push(name);
-res.send(JSON.stringify(names));
-});
+
 
 
 
