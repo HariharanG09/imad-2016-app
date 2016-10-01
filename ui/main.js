fdb75e0991fb0a1cfd710cf38694cfaf54f7019e
HariharanG09 <valavanurguru@gmail.com>
2016-10-01 19:49:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,18 @@
 //counter
 var button=document.getElementById("counter");
-var counter=0;
 button.onclick=function(){
- counter=counter+1;
+    var request=new XMLHttpRequest();
+    request.onreadyststechange=function(){
+      if(request.readyState==XMLHttpRequest.DONE){
+          if(request.status==200){
+             var counter=request.responseText;
+         counter=counter+1;
  var span=document.getElementById("count");
  
  span.innerHTML=counter.toString();
+          }
+      }  
+    };
+ request.open("GET","http://hariharang09.imad.hasura-app.io/counter",true)
+ request.send(numm);
 };
